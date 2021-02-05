049731faff31f84ca8964a4d2205927f8cf5e98d
David Nestle <david.nestle@iee.fraunhofer.de>
2021-02-05 13:20:38 +0100

    fix finding device resource

--- backup-gateways-analysis/src/main/java/org/smartrplace/analysis/backup/parser/util/MemoryResourceUtilSimple.java
+++ backup-gateways-analysis/src/main/java/org/smartrplace/analysis/backup/parser/util/MemoryResourceUtilSimple.java
@@ -124,8 +124,9 @@ public class MemoryResourceUtilSimple {
 		return MemoryResourceUtilSimple.getResource(memGw, parentPath);
 	}
 	
-	/** Get a device that is a super-resource of res that contains room information
-	 * 
+	/** Get a device that is a super-resource of res that contains room information.
+	 *  Get device with room if possible, other device without room. It two results are possible return most top-level device
+	 *  as we usually do not want to separate into sub-devices.
 	 * @param res
 	 * @param preData
 	 * @return
@@ -134,16 +135,22 @@ public class MemoryResourceUtilSimple {
 		String path = res.getPath();
 		Resource result = null;
 		for(Entry<String, Resource> e: preData.roomByDevice.entrySet()) {
-			if(path.startsWith(e.getKey()))
+			if(path.startsWith(e.getKey())) {
+				if((path.length() > e.getKey().length()) && (path.charAt(e.getKey().length()-1) != '/') && (path.charAt(e.getKey().length()) != '/'))
+					continue;
 				if(result == null || result.getPath().length() > e.getKey().length())
 					result = preData.allDevices.get(e.getKey());
+			}
 		}
 		if(result != null)
 			return result;
 		for(Entry<String, Resource> e: preData.allDevices.entrySet()) {
-			if(path.startsWith(e.getKey()))
+			if(path.startsWith(e.getKey())) {
+				if((path.length() > e.getKey().length()) && (path.charAt(e.getKey().length()-1) != '/') && (path.charAt(e.getKey().length()) != '/'))
+					continue;
 				if(result == null || result.getPath().length() > e.getKey().length())
-					result = e.getValue();			
+					result = e.getValue();
+			}
 		}
 		return result;
 	}
